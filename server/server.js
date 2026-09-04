1057e12afdf3543c2e03a0d16b6a9a5c2c003542
Bhaktesh Patel <bhaktesh9@gmail.com>
2026-09-04 19:25:34 +0300

    Prepare backend for production hosting

--- server/server.js
+++ server/server.js
@@ -116,6 +116,6 @@ app.get("/api/matches/:matchId/squad", async (req, res) => {
 // START SERVER
 // ------------------------------------
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
